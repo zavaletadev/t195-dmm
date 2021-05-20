99ce491302e3208d69928f63bb030c55595a6a4a
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-05-20 16:55:43 -0500

    Errores ortograffía

--- EjemploClases/practica3.js
+++ EjemploClases/practica3.js
@@ -1,18 +1,18 @@
 /**
- * Generar un micro poryecto que contenga las siguientes clases:
- * Clase Persona ---- Almacena el nombre, apellidos, fecha de nacimiento
- *                    y edad
- * Clase Alumno ----- Almacena la matricula, la carrera y el grupo de un 
- *                    alumno que a su vez es una persona (extends)
- * Clase Materia ---- Almacena el nombre de la materia, cuatrimestre en 
- *                    que se imparte y el profesor que la imparte
- * Clase Kardex ----- El kardex contiene un nuevo alumno con todos sus datos.
- * 
- *                    Agregar un método que genere un arreglo de materias, para
- *                    poder agregar todas las materias del alumno.
- * 
- *                    Agregar otro metodo que genere la impresión de un kardex
- *                    mostrando los datos del alumno así como la tira de materias
- *                    con su respectiva calificación
- *                  
- */
+* Generar un micro proyecto que contenga las siguientes clases:
+* Clase Persona ---- Almacena el nombre, apellidos, fecha de nacimiento
+* y edad
+* Clase Alumno ----- Almacena la matricula, la carrera y el grupo de un
+* alumno que a su vez es una persona (extends)
+* Clase Materia ---- Almacena el nombre de la materia, cuatrimestre en
+* que se imparte y el profesor que la imparte
+* Clase Kardex ----- El kardex contiene un nuevo alumno con todos sus datos.
+*
+* Agregar un método que genere un arreglo de materias, para
+* poder agregar todas las materias del alumno.
+*
+* Agregar otro método que genere la impresión de un kardex
+* mostrando los datos del alumno así como la tira de materias
+* con su respectiva calificación
+*
+*/
