1fe91c2a31a8c10dba35f1a8c3d6e1e1ccbfbe80
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-05-31 14:06:31 -0500

    Ejemplo de estilos sobre componentes e imágenes

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import {
+	Button,
+	Image,
+	SafeAreaView,
+	ScrollView,
+	StatusBar,
+	Switch,
+	Text,
+	View,
+} from 'react-native';
 import TableroAjedrez from './src/components/TableroAjedrez';
+
+import { Entypo } from '@expo/vector-icons';
 
 // const App = () => {
 // 	return (
@@ -83,8 +94,89 @@ CSS
 3.- Crear un componente externo de estilos
 */
 
+// const App = (props) => {
+// 	return <TableroAjedrez />;
+// };
+
 const App = (props) => {
-	return <TableroAjedrez />;
+	return (
+		<View style={{ flex: 1 }}>
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: '#185adb',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				{/**
+				 * Imagenes:
+				 * Siempre agregar ancho y alto
+				 *
+				 * Imagen a partir de una URL
+				 */}
+				{/* <Image
+					source={{
+						uri: 'https://patijinich.com/es/wp-content/uploads/sites/3/2019/08/804-tacos-campechanos-de-suadero-longaniza-y-chicharron-con-salsa-callejera.jpg',
+					}}
+					style={{
+						width: 150,
+						height: 150,
+					}}
+				/> */}
+				{/**
+				 * Imagen a partir de un archivo local
+				 */}
+				<Image
+					source={require('./assets/media/images/fotoperfil.jpg')}
+					style={{
+						width: 150,
+						height: 150,
+						resizeMode: 'cover',
+						borderRadius: 75,
+						marginBottom: 20,
+					}}
+				/>
+				<Text
+					style={{
+						fontSize: 22,
+						color: '#feddbe',
+						marginBottom: 20,
+					}}
+				>
+					Raúl Zavaleta Zea
+				</Text>
+
+				<Text
+					style={{
+						fontSize: 26,
+						fontWeight: 'bold',
+						color: '#ffc947',
+					}}
+				>
+					Profe &amp; Arquitecto de Apps
+				</Text>
+			</View>
+			<View
+				style={{
+					flex: 1,
+					backgroundColor: '#ffc947',
+				}}
+			>
+				<Text style={{ fontSize: 33 }}>
+					<Entypo
+						name='old-phone'
+						size={33}
+						color='#0a1931'
+					/>
+					{'\n'}
+					442 2048329
+				</Text>
+
+				<Button title='Llamar' color='#0000FF' />
+			</View>
+		</View>
+	);
 };
 
 export default App;
