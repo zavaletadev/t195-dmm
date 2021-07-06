477333037e9b3a0c6002688f08ab82c8b153b564
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-07-06 11:55:15 -0500

    Parte 1 abc de generos y peliculas

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from './src/screens/public/Login';
 import Registro from './src/screens/public/Registro';
 import HomeDrawer from './src/screens/private/HomeDrawer';
+import ListaGeneros from './src/screens/public/ListaGeneros';
+import { Text, TouchableOpacity } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import AgregarGenero from './src/screens/public/AgregarGenero';
 
 const Stack = createStackNavigator();
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Login'>
+			{/* <Stack.Navigator initialRouteName='Login'> */}
+			<Stack.Navigator initialRouteName='ListaGeneros'>
 				<Stack.Screen
 					name='Login'
 					component={Login}
@@ -22,6 +27,39 @@ export default function App() {
 				<Stack.Screen
 					name='HomeDrawer'
 					component={HomeDrawer}
+				/>
+				<Stack.Screen
+					options={(propsScreen) => ({
+						title: 'Géneros',
+						headerRight: () => (
+							<TouchableOpacity
+								style={{
+									paddingHorizontal: 20,
+									paddingVertical: 10,
+								}}
+								onPress={() =>
+									propsScreen.navigation.navigate(
+										'AgregarGenero'
+									)
+								}
+							>
+								<MaterialIcons
+									name='library-add'
+									color='tomato'
+									size={28}
+								/>
+							</TouchableOpacity>
+						),
+					})}
+					name='ListaGeneros'
+					component={ListaGeneros}
+				/>
+				<Stack.Screen
+					options={{
+						title: '+ Género',
+					}}
+					name='AgregarGenero'
+					component={AgregarGenero}
 				/>
 			</Stack.Navigator>
 		</NavigationContainer>
---
+++ src/screens/public/AgregarGenero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AgregarGenero = (props) => {
+	return (
+		<View
+			style={{
+				flex: 1,
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Text>AgregarGenero.js</Text>
+		</View>
+	);
+};
+
+export default AgregarGenero;
