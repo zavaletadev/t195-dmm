60b1b60e5d50b471881f76a565aea0afcbfb4551
Raúl Zavaleta <raul.zavaletazea@gmail.com>
2021-07-06 12:59:25 -0500

    Parte 3 abc de generos y peliculas

--- src/screens/public/AgregarGenero.js
+++ src/screens/public/AgregarGenero.js
@@ -1,16 +1,76 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+	Alert,
+	Button,
+	TextInput,
+	View,
+} from 'react-native';
+import firebase from './../../backend/firebase';
 
 const AgregarGenero = (props) => {
+	const [genero, setGenero] = useState('');
+
+	/*
+    Inserción de un documento en una colección
+    con un id personalizado
+    */
+	const insertaDocId = async () => {
+		await firebase.database
+			.collection('generos')
+			.doc('CG-156789')
+			.set({
+				nombre: genero,
+			});
+		console.log('Agregado');
+	};
+
+	/*
+    Inserción de un documento con un id 
+    gestionado por FireStore
+    */
+	const insertaDoc = async () => {
+		const nuevoDoc = await firebase.database
+			.collection('generos')
+			.add({
+				nombre: genero,
+			});
+		Alert.alert(`Doc: ${nuevoDoc.id} insertado`);
+	};
+
 	return (
 		<View
 			style={{
 				flex: 1,
 				justifyContent: 'center',
 				alignItems: 'center',
+				marginHorizontal: 10,
 			}}
 		>
-			<Text>AgregarGenero.js</Text>
+			<TextInput
+				style={{
+					paddingVertical: 10,
+					paddingHorizontal: 20,
+					borderWidth: 2,
+					borderColor: 'tomato',
+					borderRadius: 5,
+					width: '100%',
+					marginBottom: 15,
+				}}
+				keyboardType='default'
+				autoCapitalize='words'
+				autoCorrect
+				maxLength={120}
+				value={genero}
+				onChangeText={(val) => setGenero(val)}
+			/>
+			<Button
+				title='+ Genero'
+				color='tomato'
+				onPress={() => {
+					//insertaDocId(); //con id propio
+					insertaDoc(); // con id automático
+				}}
+			/>
 		</View>
 	);
 };
